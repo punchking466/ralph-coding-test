dc759406511c9671a6cbb9138ce197015fdbec47
Ralph <punchking466@naver.com>
2025-11-04 17:32:26 +0900

    [level 2] Title: 할인 행사, Time: 68.66 ms, Memory: 39.7 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,36 +1,32 @@
 function solution(want, number, discount) {
-    
-    const totalCount = number.reduce((acc,cur)=>{return acc+=cur},0);
+    const totalCount = number.reduce((acc,cur)=>{return acc+cur},0);
     const discountDay = discount.length;
-
+    const wantMap = new Map();
+    
+    for(let i=0; i<want.length; i++){
+        wantMap.set(want[i], number[i]);
+    }
+    
     let count = 0;
     
-    const wantMapIndex = want.reduce((acc,cur,idx)=>{
-       return{
-           ...acc,
-           [cur]: idx+1,
-       } 
-    },{});
-    console.log(wantMapIndex)
-    
-    const arr = Array.from({length:number.length}).fill(0);
-    
-    for(let j=0; j<= discount.length - totalCount; j++){
-        for(let i=j; i<j+totalCount; i++){
-            const index = wantMapIndex[discount[i]];
-            if(!index) continue;
-
-            arr[index-1]++;
+    for(let i=0; i<=discountDay-totalCount; i++){
+        const sliced = discount.slice(i, i+totalCount);
+        const counter = new Map();
+        
+        for(let item of sliced){
+            counter.set(item, (counter.get(item)||0)+1);
         }
         
-        let result = true;
-        for(let i=0; i<arr.length; i++){
-            if(arr[i] !== number[i]) result =false;
-            arr[i] = 0;
+        let isMatch = true;
+        
+        for(let [key, val] of wantMap.entries()){
+            if(counter.get(key) !== val){
+                isMatch= false;
+                break;
+            }
         }
-        if(result){
-            count++;
-        }
+        
+        if(isMatch) count++;
     }
 
     return count;
