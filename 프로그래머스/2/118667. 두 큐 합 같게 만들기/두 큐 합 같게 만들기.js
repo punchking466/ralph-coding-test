c86ed9f77d241e1336a395f83ad6dc324f4ba053
Ralph <punchking466@naver.com>
2025-11-06 16:21:12 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 35.03 ms, Memory: 40.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,67 +1,30 @@
 function solution(queue1, queue2) {
-    const cQueue1 = new Queue();
-    const cQueue2 = new Queue();
+    const q = [...queue1, ...queue2];
+    const len = queue1.length;
+    
     let sum1 = 0;
     let sum2 = 0;
     
-    if((sum1 + sum2)%2 !== 0) return -1;
-    
     for(let i=0; i<queue1.length; i++){
-        sum1 += queue1[i];
-        sum2 += queue2[i];
-        cQueue1.push(queue1[i]);
-        cQueue2.push(queue2[i]);
+        sum1+=queue1[i];
+        sum2+=queue2[i];
     }
+    let total = sum1 + sum2;
+    const target = total/2;
+    if(total%2 !== 0) return;
     
-    const avg = Math.floor((sum1 + sum2)/2);
+    let i = 0;
+    let j = len;
     
-    let count =0;
-    let tryCount= 0;
-    while(sum1 !== avg ){
-        if(tryCount > queue1.length *3) return -1;
-         
-        let num = 0;
-        if(sum1 > sum2){
-            num = cQueue1.shift();
-            if(cQueue1.size() === 0 && num > sum2) return -1;
-            cQueue2.push(num);
-            sum2 += num;
-            sum1 -= num;
+    let count = 0;
+    while(count < len*4){
+        if(sum1 === target) return count;
+        if(sum1 > target){
+            sum1 -= q[i++];
         }else{
-            num = cQueue2.shift();
-            if(cQueue2.size() === 0 && num > sum1) return -1;
-            cQueue1.push(num);
-            sum1 += num;
-            sum2 -= num;
+            sum1 += q[j++];
         }
         count++;
-        tryCount++;
     }
-    return count;
-}
-
-class Queue {
-    constructor(){
-        this.items=[];
-        this.head = 0;
-    }
-    
-    size(){
-        return this.items.length - this.head;
-    }
-    
-    push(item){
-        this.items.push(item);
-    }
-    
-    shift(){
-        if(this.items.length === 0) return undefined;
-        const item = this.items[this.head];
-        this.head++;
-        if(this.items.length === this.head){
-            this.items = [];
-            this.head = 0;
-        }
-        return item;
-    }
+    return -1;
 }
