ba2658378338a86acd7067ebffb4db98c31dceb2
Ralph <punchking466@naver.com>
2025-11-06 12:55:10 +0900

    [level 2] Title: 더 맵게, Time: 570.21 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.js
@@ -0,0 +1,100 @@
+function solution(scoville, K) {
+    const minHeap = new MinHeap();
+    for(let item of scoville){
+        minHeap.push(item);
+    }
+    let count = 0;
+    while(minHeap.size() >=2){
+        const food1 = minHeap.pop();
+        if(food1 >= K){
+            return count; 
+        }
+        const food2 = minHeap.pop();
+        minHeap.push(food1+ (food2*2));
+        count++;
+    }
+    
+    if(minHeap.size() === 1 && minHeap.pop() >= K){
+        return count;
+    }
+    
+    return -1;
+}
+
+class MinHeap {
+  constructor() {
+    this.heap = [];
+  }
+
+  push(value) {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  pop() {
+    if (this.isEmpty()) return null;
+
+    const root = this.heap[0];
+    const lastNode = this.heap.pop();
+
+    if (!this.isEmpty()) {
+      this.heap[0] = lastNode;
+      this.heapifyDown();
+    }
+
+    return root;
+  }
+
+  isEmpty() {
+    return this.heap.length === 0;
+  }
+    size() {
+        return this.heap.length;
+    }
+
+  heapifyUp() {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      const parentIndex = Math.floor((index - 1) / 2);
+      if (this.heap[parentIndex] <= this.heap[index]) break;
+      [this.heap[parentIndex], this.heap[index]] = [
+        this.heap[index],
+        this.heap[parentIndex],
+      ];
+      index = parentIndex;
+    }
+  }
+
+  heapifyDown() {
+    let index = 0;
+    const length = this.heap.length;
+
+    while (true) {
+      let smallest = index;
+      const leftChildIndex = 2 * index + 1;
+      const rightChildIndex = 2 * index + 2;
+
+      if (
+        leftChildIndex < length &&
+        this.heap[leftChildIndex] < this.heap[smallest]
+      ) {
+        smallest = leftChildIndex;
+      }
+
+      if (
+        rightChildIndex < length &&
+        this.heap[rightChildIndex] < this.heap[smallest]
+      ) {
+        smallest = rightChildIndex;
+      }
+
+      if (smallest === index) break;
+
+      [this.heap[index], this.heap[smallest]] = [
+        this.heap[smallest],
+        this.heap[index],
+      ];
+      index = smallest;
+    }
+  }
+}
