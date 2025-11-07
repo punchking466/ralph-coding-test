f5230d49323923f41bc1ce496e5239fcdb6c0416
Ralph <punchking466@naver.com>
2025-11-07 16:09:34 +0900

    [level 3] Title: 네트워크, Time: 4.73 ms, Memory: 36.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,27 +1,24 @@
 function solution(n, computers) {
-    if(n === 1) return 1;
-    
-    let count = 0;
+    if(n===1) return n;
     const visited = new Set();
+    let count = 0;
+    
     for(let i=0; i<n; i++){
-        if(!visited.has(i)){
-            count++;
+        if(visited.has(i)) continue;
+        const stack = [i];
+        count++;
+        while(stack.length> 0){
+            const node = stack.pop();
             
-            const stack = [i];
-        
-            while(stack.length> 0){
-                const node = stack.pop();
-                if(visited.has(node)) continue;
-                visited.add(node);
-
-                for(let j=0; j<n; j++){
-                    if(computers[node][j] === 1 && !visited.has(j)){
-                        stack.push(j)
-                    }
+            visited.add(node);
+            
+            for(let j=0; j<n; j++){
+ 
+                if(computers[node][j] === 1 && !visited.has(j)){
+                    stack.push(j);
                 }
             }
         }
-        
     }
     return count;
 }
