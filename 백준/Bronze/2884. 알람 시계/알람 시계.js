69ece5faecbeac64398209865736f2d55d0cd318
Ralph <punchking466@naver.com>
2025-12-02 15:51:38 +0900

    [Bronze III] Title: 알람 시계, Time: 96 ms, Memory: 9336 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.js
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -1,10 +1,7 @@
 function solution(input) {
   const [h, m] = input.split(" ").map(Number);
-  const formatMinute = h * 60 + m;
-  const alarm = formatMinute - 45;
-  const alarmH = Number(Math.floor(alarm / 60));
-  const alarmM = Number(alarm % 60);
-  return `${alarmH < 0 ? 23 : alarmH} ${alarmM < 0 ? 60 + alarmM : alarmM}`;
+  const total = (h * 60 + m + 1440 - 45) % 1440;
+  return `${Math.floor(total / 60)} ${total % 60}`;
 }
 
 let input = require('fs').readFileSync(0, 'utf-8').trim();
