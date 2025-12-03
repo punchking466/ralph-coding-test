34ea0e11d4993a464fb6dd5210d992967c0d9053
Ralph <punchking466@naver.com>
2025-12-03 14:07:11 +0900

    [Bronze V] Title: X보다 작은 수, Time: 100 ms, Memory: 11732 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.js
@@ -3,14 +3,14 @@ function solution(input) {
   const [N, target] = lines[0].split(" ").map(Number);
   const testCases = lines[1].split(" ").map(Number);
 
-  let result = "";
+  let result = [];
 
   for (let i = 0; i < N; i++) {
     if (testCases[i] < target) {
-      result += `${testCases[i]} `;
+      result.push(testCases[i]);
     }
   }
-  return result.trim();
+  return result.join(" ");
 }
 
 let input = require('fs').readFileSync(0, 'utf-8').trim();
