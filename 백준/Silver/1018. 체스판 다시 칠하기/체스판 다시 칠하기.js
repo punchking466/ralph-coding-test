eb80918575eeb02bcfe681c0752a33e0fe3b223d
Ralph <punchking466@naver.com>
2025-12-17 09:52:10 +0900

    [Silver III] Title: 체스판 다시 칠하기, Time: 160 ms, Memory: 10360 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.js
@@ -1,46 +1,28 @@
 function solution(input) {
   const lines = input.split("\n");
-  const [N, M] = lines.shift().split(" ");
-  const tiles = lines;
+  const [N, M] = lines.shift().split(" ").map(Number);
+  const board = lines;
+  let minCount = Infinity;
 
-  let min = Infinity;
-  let count = 0;
   for (let r = 0; r <= N - 8; r++) {
     for (let c = 0; c <= M - 8; c++) {
-      ["B", "W"].forEach((t) => {
-        const evenTile = t;
-        const oddTile = t === "W" ? "B" : "W";
-        for (let i = r; i < r + 8; i++) {
-          for (let j = c; j < c + 8; j++) {
-            if (i % 2 === r % 2) {
-              let tile = "";
-              if (c % 2 === j % 2) {
-                tile = evenTile;
-              } else {
-                tile = oddTile;
-              }
-              if (tile !== tiles[i][j]) {
-                count++;
-              }
-            } else {
-              let tile = "";
-              if (c % 2 === j % 2) {
-                tile = oddTile;
-              } else {
-                tile = evenTile;
-              }
-              if (tile !== tiles[i][j]) {
-                count++;
-              }
-            }
-          }
+      let countW = 0;
+      let countB = 0;
+
+      for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+          const cur = board[r + i][c + j];
+          const expectedW = (i + j) % 2 === 0 ? "W" : "B";
+          const expectedB = (i + j) % 2 === 0 ? "B" : "W";
+
+          if (cur !== expectedW) countW++;
+          if (cur !== expectedB) countB++;
         }
-        min = min > count ? count : min;
-        count = 0;
-      });
+      }
+      minCount = Math.min(minCount, countW, countB);
     }
   }
-  return min;
+  return minCount;
 }
 
 let input = require('fs').readFileSync(0, 'utf-8').trim();
