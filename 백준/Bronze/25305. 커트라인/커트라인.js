74a9d091291861671e70087e54b93ba0f021f13c
Ralph <punchking466@naver.com>
2025-12-18 09:38:33 +0900

    [Bronze II] Title: 커트라인, Time: 92 ms, Memory: 9388 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.js
+++ 백준/Bronze/25305. 커트라인/커트라인.js
@@ -1,27 +1,12 @@
 function solution(input) {
   const lines = input.split("\n");
   const [N, k] = lines[0].split(" ").map(Number);
-  const scores = quickSort(lines[1].split(" ").map(Number));
-  return scores[N - k];
+  const scores = lines[1]
+    .split(" ")
+    .map(Number)
+    .sort((a, b) => b - a);
+
+  return scores[k - 1];
 }
-function quickSort(arr) {
-  if (arr.length <= 1) {
-    return arr;
-  }
-  const pivot = arr[0];
-  const left = [];
-  const right = [];
-
-  for (let i = 1; i < arr.length; i++) {
-    if (arr[i] > pivot) {
-      right.push(arr[i]);
-    } else {
-      left.push(arr[i]);
-    }
-  }
-
-  return [...quickSort(left), pivot, ...quickSort(right)];
-}
-
 let input = require('fs').readFileSync(0, 'utf-8').trim();
 console.log(solution(input));
