fd1a4a47ddf72d2051bf2163009685bd76a7e9ef
Ralph <punchking466@naver.com>
2026-01-06 17:08:34 +0900

    [Gold IV] Title: 문자열 폭발, Time: 1116 ms, Memory: 124824 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.js
@@ -1,17 +1,20 @@
 function solution(input) {
   const str = input.split("\n")[0].split("");
   const bomb = input.split("\n")[1];
-  const len = bomb.length;
+  const bombLen = bomb.length;
   const stack = [];
 
   for (let i = 0; i < str.length; i++) {
     stack.push(str[i]);
-    while (str.length >= len && stack.slice(-len).join("") === bomb) {
-      stack.length = stack.length - len;
+
+    if (stack.length >= bombLen && stack.slice(-bombLen).join("") === bomb) {
+      stack.length -= bombLen;
     }
   }
 
   return stack.length > 0 ? stack.join("") : "FRULA";
 }
+
+
 let input = require('fs').readFileSync(0, 'utf-8').trim();
 console.log(solution(input));
