f29ecca77edffd1d5e1e5739891027474af9c53d
Ralph <punchking466@naver.com>
2026-01-06 16:00:36 +0900

    [Gold IV] Title: 오큰수, Time: 736 ms, Memory: 192508 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.js
+++ 백준/Gold/17298. 오큰수/오큰수.js
@@ -2,18 +2,18 @@ function solution(input) {
   const n = Number(input.split("\n")[0]);
   const nums = input.split("\n")[1].split(" ").map(Number);
 
-  const result = [];
+  const result = Array(n).fill(-1);
   const stack = [];
 
-  for (let j = n - 1; j >= 0; j--) {
-    while (stack.length > 0 && stack[stack.length - 1] <= nums[j]) {
-      stack.pop();
-    }
-    result.push(stack.length > 0 ? stack[stack.length - 1] : -1);
-    stack.push(nums[j]);
-  }
-  return result.reverse().join(" ");
-}
+  for (let i = 0; i < n; i++) {
+    const cur = nums[i];
 
+    while (stack.length > 0 && nums[stack[stack.length - 1]] < cur) {
+      result[stack.pop()] = cur;
+    }
+    stack.push(i);
+  }
+  return result.join(" ");
+}
 let input = require('fs').readFileSync(0, 'utf-8').trim();
 console.log(solution(input));
