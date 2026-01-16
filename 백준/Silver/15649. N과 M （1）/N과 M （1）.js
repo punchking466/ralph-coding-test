029bec468f57a7d453bda7191fba5944bee44ec9
Ralph <punchking466@naver.com>
2026-01-16 16:50:30 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 23024 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -0,0 +1,33 @@
+
+function solution(input) {
+  const [N, M] = input.trim().split(" ").map(Number);
+  const used = Array(N + 1).fill(false);
+
+  const result = [];
+  function dfs(items) {
+    if (items.length === M) {
+      result.push(items.join(" "));
+      return;
+    }
+
+    for (let i = 1; i <= N; i++) {
+      if (used[i]) continue;
+      used[i] = true;
+      dfs([...items, i]);
+      used[i] = false;
+    }
+  }
+
+  dfs([]);
+  return result.join("\n");
+}
+
+let input = "";
+
+try {
+  input = require("fs").readFileSync("input.txt", "utf-8").trim();
+} catch (e) {
+  input = require("fs").readFileSync(0, "utf-8").trim();
+}
+
+console.log(solution(input));
